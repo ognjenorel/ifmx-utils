ba4d45efebddcc537694ad5333abe3e416b7aa05
Ognjen Orel <ognjenorel@users.noreply.github.com>
2015-11-29 19:32:56 +0100

    added license info and link

--- sql/rowLevelAuditing.sql
+++ sql/rowLevelAuditing.sql
@@ -1,3 +1,23 @@
+4 lines (44 sloc) 1.92 KB
+-- Copyright 2015 Ognjen Orel
+-- See more at: https://ifmx.wordpress.com/2015/11/29/simple-and-meaningful-row-level-auditing/
+--
+-- This file is part of ifmx utilities.
+--
+-- ifmx utilities is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- IFMX Table copy utility is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with ifmx utilities. If not, see <http://www.gnu.org/licenses/>.
+
+
 CREATE VIEW vsyscolumns (tabname, colname, colno, type) AS
 SELECT systables.tabname
      , syscolumns.colname
